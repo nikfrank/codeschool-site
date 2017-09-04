88e08df08b7b23161a92ba718b5d1194e8fc57c8
nik frank <nikbfrank@gmail.com>
2017-09-04 23:41:38 +0300

    heroku domain

--- src/network/LoadCurriculum/LoadCurriculum.js
+++ src/network/LoadCurriculum/LoadCurriculum.js
@@ -4,14 +4,14 @@ import parseInstructions from './parse';
 const { fetch } = global;
 
 
-//const domain = 'https://rawgit.com/nikfrank';
+const domain = 'https://rawgit.com/nikfrank';
 
-const domain = 'http://localhost:8000';
+//const domain = 'http://localhost:8000';
 
 const course = 'learn-tahini-mastermind';
 
-// const filename = 'master/docs/instructions.md';
-const filename = 'docs/instructions.md';
+const filename = 'master/docs/instructions.md';
+// const filename = 'docs/instructions.md';
 
 export default class LoadCurriculum {
   constructor(next, done, err, { Fetcher }){
